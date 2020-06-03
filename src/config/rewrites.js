9e708e77b232d0705f5958576c8866f8dcaf479e
mbabic84 <martinbabic84@gmail.com>
2020-06-03 15:54:49 +0200

    Update src/config/rewrites.js
    Co-authored-by: tmdmc <tmdmc@users.noreply.github.com>

--- src/config/rewrites.js
+++ src/config/rewrites.js
@@ -1,2 +1,11 @@
-module.exports = {
+/**
+ * CONFIG
+ *
+ * Per-instance values, development values & features.
+ * See documentation in ./defaults.js
+ * Versions/variants managed in github.com/gisat/docker/
+ */
+
+export default {
+
 };
